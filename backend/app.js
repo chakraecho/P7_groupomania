fe613a8f5c05e59c5c97101bdcb0cbbf98f795b6
Julien Huang <julien.huang@outlook.fr>
2020-08-09 10:54:45 +0200

    feat(backend):added server and app -added express dependency -added bodyparser dependecy -added helmet dependency -set cors rules -set helmet -set bodyparser

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const helmet = require('helmet')
+
+
+const app = express()
+
+app.use(helmet())
+app.use(bodyparser)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+
+
+
+module.exports = app
