68d918e5040ae7367b0647d03bc4c55b4845f072
Julien Huang <julien.huang@outlook.fr>
2020-09-03 09:19:35 +0200

    feat(group): added groups and groupMembers controller

---
+++ backend/controllers/group.js
@@ -0,0 +1,60 @@
+const User = require('./../models/users')
+const {groups, groupMembers} = require('./../models/group')
+
+exports.createGroup = (req, res, next)=>{
+    const groupName = req.body.groupName
+    const user = req.body.userId
+    const description = req.body.description
+
+    groups.create({
+        groupName,
+        description,
+        userId: user
+    }).then(group => groupMembers.create({
+        groupId: group.groupId,
+        userId: user
+    })
+        .catch(error=> res.status(500).json({message:'groupe non trouvé !', error}))
+    )
+    .catch(error=> res.status(500).json({error}))
+}
+
+exports.modifyGroup= (req,res)=>{
+    const groupName = req.body.groupName
+    const description = req.body.description
+    const groupId = req.params.id
+
+    groups.update({description,}, {where: {groupId}})
+        .then(()=> res.status(200).json({message:'groupe modifié !'}))
+        .catch(()=> res.status(500).json({error}))
+}
+
+exports.modifyImg = (req,res)=>{
+    const groupId = req.params.id
+    groups.update({imgUrl: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`}, {where:{groupId}})
+        .then(()=> res.status(200).json({message:'image de groupe modifié !'}))
+        .catch((error)=> res.status(500).json({error}))
+}
+
+exports.modifyBanner = (req,res)=>{
+    const groupId = req.params.id
+    groups.update({bannerUrl: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`}, {where:{groupId}})
+    .then(()=> res.status(200).json({message:'image de groupe modifié !'}))
+    .catch((error)=> res.status(500).json({error}))
+}
+
+exports.deleteGroup = (req,res)=>{
+    const groupId = req.params.id
+    groups.destroy({where:{groupId}})
+        .then(()=> res.status(200).json({message:'groupe supprimé !'}))
+}
+
+
+exports.addMember = (req, res)=>{
+    const userId = req.body.userId
+    const groupId = req.params.id
+
+    groupMembers.create({userId, groupId})
+        .then(()=> res.status(200).json({message:'vous avez rejoint le groupe !'}))
+        .catch(error => res.status(500).json({error}))
+}
--- backend/models/group.js
+++ backend/models/group.js
@@ -5,8 +5,9 @@ const Sequelize = require('sequelize')
 const groups = sequelize.define('groups',{
     groupId:{type:Sequelize.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true},
     description:{type:Sequelize.TEXT, allowNull:false},
-    imgUrl:{type:Sequelize.STRING },
-    bannerUrl:{type:Sequelize.STRING}
+    imgUrl:{type:Sequelize.STRING, default:'http://localhost:3000/assets/group.svg'},
+    bannerUrl:{type:Sequelize.STRING, default:'http://localhost:3000/assets/default_banner.svg'},
+    groupName:{type:Sequelize.STRING, allowNull:false}
 })
 
 const groupMembers = sequelize.define('groupMembers')
