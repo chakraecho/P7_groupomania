508fdfd4b8ed8bbf422006eb288c7259fe30ea92
Julien Huang <julien.huang@outlook.fr>
2020-09-06 17:35:56 +0200

    feat(Routes): added group routes

--- backend/routes/group.js
+++ backend/routes/group.js
@@ -5,5 +5,10 @@ const groupCtrl = require('./../controllers/group')
 
 router.post('/', groupCtrl.createGroup)
 router.post('/:id', groupCtrl.addMember)
+router.put('/:id', groupCtrl.modifyGroup)
+router.put('/:id/img', groupCtrl.modifyImg)
+router.put('/:id/banner', groupCtrl.modifyBanner)
+
+router.delete('/:id', groupCtrl.deleteGroup)
 
 module.exports = router
