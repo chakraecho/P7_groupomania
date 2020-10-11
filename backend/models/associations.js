170fe31d32e72d8b0dc8a797f4de8ffe0fac6662
julien huang <me@j-huang.fr>
2020-10-11 12:06:35 +0200

    fix(association): fixed all associations

--- backend/models/associations.js
+++ backend/models/associations.js
@@ -1,7 +1,7 @@
 const { Post, Comments, userLiked, commentLiked } = require('./post')
 const User = require('./users')
 const noctification = require('./noctification.js')
-const { groups, groupMembers } = require('./group')
+const { groups, groupMembers, groupRole } = require('./group')
 const department = require('./department')
 const follow = require('./follow')
 
@@ -16,55 +16,30 @@ commentLiked.belongsTo(Comments, { foreignKey: { name: 'id', allowNull: false } 
 Post.belongsTo(User, { foreignKey: { name: 'userId', allowNull: false } })
 
 
-userLiked.belongsTo(User, { allowNull: false, foreignKey: 'userId' })
-userLiked.belongsTo(Post, { allowNull: false, foreignKey: 'postId' })
+userLiked.belongsTo(User, { allowNull: false, foreignKey: { name: 'userId' } })
+userLiked.belongsTo(Post, { allowNull: false, foreignKey: { name: 'postId' } })
 
-noctification.belongsTo(User, {
-    as: 'creator',
-    foreignKey: {
-        name: 'creator_id'
-    }
-})
-noctification.belongsTo(User, {
-    as: 'notified',
-    foreignKey: {
-        name: 'notified_id'
-    }
-})
-noctification.belongsTo(groups, {
-    foreignKey: {
-        name: 'groupId'
-    }
-})
+noctification.belongsTo(User, {as: 'creator',foreignKey: {name: 'creator_id'}})
+noctification.belongsTo(User, {as: 'notified',foreignKey: {name: 'notified_id'}})
+noctification.belongsTo(groups, {foreignKey: { name: 'groupId' }})
+
+groupMembers.belongsTo(groupRole, { foreignKey: { name: 'role'}})
 
 follow.belongsTo(User, { foreignKey: 'follower' })
 follow.belongsTo(User, { foreignKey: 'followed' })
 
-groupMembers.belongsTo(User, { foreignKey: 'userId', allowNull: false })
-groupMembers.belongsTo(groups, { foreignKey: 'groupId', allowNull: false })
+groupMembers.belongsTo(User, { foreignKey: { name: 'userId' }, allowNull: false })
+groupMembers.belongsTo(groups, { foreignKey: { name: 'groupId' }, allowNull: false })
 
 
-Comments.hasMany(commentLiked)
-groups.hasMany(groupMembers)
-Post.hasMany(userLiked)
+Comments.hasMany(commentLiked,{ foreignKey: { name: 'id', allowNull: false }})
+groups.hasMany(groupMembers, { foreignKey: { name: 'groupId' }, allowNull: false })
+Post.hasMany(userLiked, { allowNull: false, foreignKey: { name: 'postId' } })
 User.hasMany(Post, { foreignKey: { name: 'userId', allowNull: false } })
-User.hasMany(groupMembers)
-User.hasMany(userLiked)
-User.hasMany(Comments)
-User.hasMany(noctification, {
-    as: 'creator',
-    foreignKey: {
-        name: 'creator_id'
-    }
-})
-User.hasMany(noctification, {
-    as: 'notified',
-    foreignKey: {
-        name: 'notified_id'
-    }
-})
-groups.hasMany(noctification, {
-    foreignKey: {
-        name: 'groupId'
-    }
-})
+User.hasMany(groupMembers, {foreignKey: { name: 'userId' }, allowNull: false})
+User.hasMany(userLiked, { allowNull: false, foreignKey: { name: 'userId' } })
+User.hasMany(Comments, { allowNull: false, foreignKey: { name: 'userId' } })
+User.hasMany(noctification, {as: 'creator',foreignKey: {name: 'creator_id'}})
+User.hasMany(noctification, {as: 'notified', foreignKey: {name: 'notified_id'}})
+groups.hasMany(noctification, {foreignKey: {name: 'groupId'}})
+groupRole.hasMany(groupMembers, {foreignKey: {name: 'role'}})
--- backend/models/group.js
+++ backend/models/group.js
@@ -2,20 +2,44 @@ const sequelize = require('./../db-config')
 const Sequelize = require('sequelize')
 
 
-const groups = sequelize.define('groups',{
-    groupId:{type:Sequelize.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true},
-    description:{type:Sequelize.TEXT, allowNull:false},
-    imgUrl:{type:Sequelize.STRING, defaultValue:'http://localhost:3000/assets/group.svg'},
-    bannerUrl:{type:Sequelize.STRING, defaultValue:'http://localhost:3000/assets/default_banner.jpg'},
-    groupName:{type:Sequelize.STRING, allowNull:false}
+const groups = sequelize.define('groups', {
+    groupId: { type: Sequelize.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
+    description: { type: Sequelize.TEXT, allowNull: false },
+    imgUrl: { type: Sequelize.STRING, defaultValue: 'http://localhost:3000/assets/group.svg' },
+    bannerUrl: { type: Sequelize.STRING, defaultValue: 'http://localhost:3000/assets/default_banner.jpg' },
+    groupName: { type: Sequelize.STRING, allowNull: false }
 })
 
-const groupMembers = sequelize.define('groupMembers',{
-    isCreator:{type:Sequelize.BOOLEAN},
-    isAdmin : {type: Sequelize.BOOLEAN, defaultValue : false}
+const groupMembers = sequelize.define('groupMembers')
+
+const groupRole = sequelize.define('groupRole', {
+    groupRoleId: { type: Sequelize.INTEGER.UNSIGNED, primaryKey: true },
+    roleName: { type: Sequelize.STRING(20) }
+}, { timestamps: false })
+
+groupRole.create({
+    groupRoleId: 1,
+    roleName: 'creator'
+},
+{
+    groupRoleId: 2,
+    roleName: 'admin'
+},
+{
+    groupRoleId: 3,
+    roleName: 'user'
+})
+groupRole.create(
+{
+    groupRoleId: 2,
+    roleName: 'admin'
+})
+groupRole.create(
+{
+    groupRoleId: 3,
+    roleName: 'user'
 })
 
-
-
+exports.groupRole = groupRole
 exports.groupMembers = groupMembers
 exports.groups = groups
