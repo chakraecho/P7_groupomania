4d9dcd61a00cbda4cadb0570c511f870e5ed8464
julien huang <me@j-huang.fr>
2020-11-01 15:23:59 +0100

    style(controllers): changed name import

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,7 +7,7 @@ const followCtrl = require('./../controllers/follow')
 const notifCtrl = require('./../controllers/noctification')
 const searchCtrl = require('./../controllers/search')
 
-const userSecurity = require('./../middleware/auth/user')
+const {isLoggedIn, isUser} = require('./../middleware/auth/user')
 
 const multer = require('./../middleware/multer-config')
 
@@ -15,24 +15,24 @@ router.post('/auth/signup', userCtrl.signup)
 router.post('/auth/login', userCtrl.login)
 router.post('/auth/verify', userCtrl.verify)
 
-router.get('/search',userSecurity.isLoggedIn, searchCtrl.searchUser)
+router.get('/search',isLoggedIn, searchCtrl.searchUser)
 
-router.put("/account/:id",userSecurity.isLoggedIn, userSecurity.isUser, userCtrl.modify)
-router.put('/account/:id/profil',userSecurity.isLoggedIn, userSecurity.isUser, multer, userCtrl.changeImg)
-router.put('/account/:id/banner',userSecurity.isLoggedIn, userSecurity.isUser, multer,  userCtrl.changeBanner)
+router.put("/account/:id",isLoggedIn, isUser, userCtrl.modify)
+router.put('/account/:id/profil',isLoggedIn, isUser, multer, userCtrl.changeImg)
+router.put('/account/:id/banner',isLoggedIn, isUser, multer,  userCtrl.changeBanner)
 
 router.get('/:id', userCtrl.getUser)
 
-router.get('/:id/post',userSecurity.isLoggedIn, postCtrl.userPost)
+router.get('/:id/post',isLoggedIn, postCtrl.userPost)
 
-router.get('/notification/:id',userSecurity.isLoggedIn, userSecurity.isUser, notifCtrl.getAll)
+router.get('/notification/:id',isLoggedIn, isUser, notifCtrl.getAll)
 
-router.get('/follow/:id',userSecurity.isLoggedIn, followCtrl.followOne)
-router.get('/follow/check/:id',userSecurity.isLoggedIn, followCtrl.checkFollow)
+router.get('/follow/:id',isLoggedIn, followCtrl.followOne)
+router.get('/follow/check/:id',isLoggedIn, followCtrl.checkFollow)
 
 
-router.delete('/follow/:id',userSecurity.isLoggedIn, followCtrl.deleteFollow)
-router.delete('/delete',userSecurity.isLoggedIn, userCtrl.delete)
+router.delete('/follow/:id',isLoggedIn, followCtrl.deleteFollow)
+router.delete('/delete',isLoggedIn, userCtrl.delete)
 router.delete('/disconnect', userCtrl.disconnect)
 
 
