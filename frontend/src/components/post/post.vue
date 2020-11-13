5f2ef03b6f0594bd828d463facd3ff5c2f0d3783
julien huang <me@j-huang.fr>
2020-11-13 17:33:13 +0100

    fix(post): fixed post not showing groups

--- frontend/src/components/post/post.vue
+++ frontend/src/components/post/post.vue
@@ -3,17 +3,16 @@
     <v-card class="position-relative">
 
       <v-card-title class="pa-0 post-header">
-        
+
         <v-row>
-                <div  class="rounded-circle img-profil" @click="$router.push('/account/' + userId)">
-            <img :src="User.profilImgUrl" :alt="'image de profil de ' + fullName" class="w-100" />
+          <div class="rounded-circle img-profil" @click="$router.push('/account/' + userId)">
+            <img :src="User.profilImgUrl" :alt="'image de profil de ' + fullName" class="w-100"/>
           </div>
-          <v-col class="py-0 ml-10 ml-md-5">
-            <p>{{ fullName }}</p>
+          <v-col class="py-0 pl-10 ml-10 ml-md-5">
+            <p>{{ fullName }} <template v-if="group">dans le groupe {{group.groupName}}</template></p>
           </v-col>
-          <v-col class="ml-auto py-0 ">
-            <p class="text--minor">le {{ date }}</p>
-          </v-col>
+
+
 
           <v-btn
               name="options"
@@ -35,27 +34,30 @@
       </v-card-text>
       <v-card-actions class="pa-0 h5">
         <v-row justify-space-between>
-          <v-col >
+          <v-col>
             <v-btn
                 name="dislike"
-              icon
-              :disabled="likefocus == true ? true : false"
-              :class="likefocus == false ? 'active-btn' : 'inactive'"
-              @click="likefocus === false ? sendLike(0) : sendLike(-1)"
+                icon
+                :disabled="likefocus == true ? true : false"
+                :class="likefocus == false ? 'active-btn' : 'inactive'"
+                @click="likefocus === false ? sendLike(0) : sendLike(-1)"
             >
               -
             </v-btn>
             {{ likes }}
             <v-btn
                 name="like"
-              icon
-              :disabled="likefocus == false ? true : false"
-              :class="likefocus == true ? 'active-btn' : 'inactive'"
-              @click="likefocus === true ? sendLike(0) : sendLike(1)"
+                icon
+                :disabled="likefocus == false ? true : false"
+                :class="likefocus == true ? 'active-btn' : 'inactive'"
+                @click="likefocus === true ? sendLike(0) : sendLike(1)"
             >
               +
             </v-btn>
           </v-col>
+
+            <p class="text--minor mt-auto mb-5">le {{ date }}</p>
+
           <v-col>
             <v-btn name="open_comment" text @click.native="openComment">
               commentaires
@@ -81,7 +83,7 @@ export default {
     },
     fullName() {
       return (
-        this._props.post.User.firstName + " " + this._props.post.User.lastName
+          this._props.post.User.firstName + " " + this._props.post.User.lastName
       );
     },
     likes() {
@@ -93,10 +95,10 @@ export default {
           return null;
         } else if (this._props.post.userLikeds.length > 0) {
           if (
-            Object.prototype.hasOwnProperty.call(
-              this._props.post.userLikeds[0],
-              "type"
-            )
+              Object.prototype.hasOwnProperty.call(
+                  this._props.post.userLikeds[0],
+                  "type"
+              )
           ) {
             return this._props.post.userLikeds[0].type;
           } else {
@@ -114,72 +116,77 @@ export default {
       if (this._props.dataId == this.$store.state.comment.postId) {
         this.$store.dispatch("comment/neutraliseComment");
       } else {
-        this.$store.dispatch("comment/setId", { postId: this._props.dataId });
+        this.$store.dispatch("comment/setId", {postId: this._props.dataId});
       }
     },
     sendLike(like) {
       fetch(process.env.VUE_APP_BACKEND + "/api/post/" + this.postId + "/like", {
         credentials: "include",
-        headers: { "Content-Type": "application/json" },
+        headers: {"Content-Type": "application/json"},
         method: "post",
-        body: JSON.stringify({ like })
+        body: JSON.stringify({like})
       }).then(response =>
-        response.json().then(res => {
-          this.$store.commit("post/UPDATE_POST", res.post);
-          if(!response.ok){
-            this.$store.dispatch("activateSnack", {color :"error", msg : res.message})
-          }
-        })
+          response.json().then(res => {
+            this.$store.commit("post/UPDATE_POST", res.post);
+            if (!response.ok) {
+              this.$store.dispatch("activateSnack", {color: "error", msg: res.message})
+            }
+          })
       );
     }
   },
-  mounted(){
-    this.$el.querySelector('.img-profil').style.height = this.$el.querySelector('.img-profil').style.width
-  }
 };
 </script>
-
 
 
 <style lang="scss" scoped>
 .active-btn {
   background-color: grey;
 }
-.post{
+
+.post {
   &-header {
-  font-size: 16px;
-}
-&--wrapper--img{
-  max-width:200px !important;
-  max-height: 200px !important;
-  border: 1px grey solid;
-  margin: auto
-}
-&--img{
-  width:100%
-}
+    font-size: 16px;
+  }
+
+  &--wrapper--img {
+    max-width: 200px !important;
+    max-height: 200px !important;
+    border: 1px grey solid;
+    margin: auto
+  }
+
+  &--img {
+    width: 100%
+  }
 }
 
 
-.text--minor{
+.text--minor {
   font-size: 15px;
+  height:max-content;
 }
 
-.img-profil{
+.img-profil {
   position: absolute;
-  top:-10px; left:-10px;
-  width:50px; height:50px; 
+  top: -10px;
+  left: -10px;
+  width: 50px;
+  height: 50px;
   overflow: hidden;
   border: 2px solid grey;
   background-color: lightcyan;
-  &:hover{
+
+  &:hover {
     cursor: pointer;
   }
 }
-.w-100{
-  width:100%;
+
+.w-100 {
+  width: 100%;
 }
-.position-relative{
-  position:relative;
+
+.position-relative {
+  position: relative;
 }
 </style>
